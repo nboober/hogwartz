d2c5560d549e84e273b1797c1e97e28fa77feefd
Nick Boober <nboober@terpmail.umd.edu>
2020-01-30 15:50:36 -0500

    created forms to add and update users. Used a partial for these forms since they are similar

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,36 @@ class StudentsController < ApplicationController
         @student = Student.find(params[:id])
     end
 
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.create(student_params)
+        redirect_to student_path(@student)
+    end
+
+    def edit
+        @student = Student.find(params[:id])
+    end
+
+    def update
+        @student = Student.find(params[:id])
+        @student.update(student_params)
+
+        redirect_to student_path(@student)
+    end
+
+    def destroy
+        @student = Student.find(params[:id])
+        @student.destroy
+        redirect_to students_path
+    end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name, :year, :house_id)
+    end
+
 end
