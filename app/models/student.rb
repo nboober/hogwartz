4fe6b3ded1623aa8b2fd19747273e917040afcb8
Nick Boober <nboober@terpmail.umd.edu>
2020-01-30 16:12:09 -0500

    added validations for students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,12 @@
 class Student < ApplicationRecord
     belongs_to :house
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates_inclusion_of :year, :in => 1..7
+
+    def full_name
+        return self.first_name + " " + self.last_name
+    end
+
 end
